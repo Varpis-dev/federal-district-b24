f23733364c81bfa684503c45f47e9f6a74e5bf85
Egor <egorka08092004@gmail.com>
2026-08-18 20:05:24 +0400

    Create calc.php

---
+++ api/calc.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__ . '/common.php';
+
+$input = json_decode(file_get_contents('php://input'), true);
+
+if (!is_array($input)) {
+  $input = $_REQUEST;
+}
+
+$city = $input['city'] ?? '';
+$region = $input['region'] ?? '';
+
+$result = calcDistrictName($city, $region);
+
+jsonResponse([
+  'success' => true,
+  'city' => $city,
+  'region' => $region,
+  'result' => $result
+]);
